131bb35965b5d0adb0fd664257dd5eedf05b161e
Max <93283693+MaksimPilat@users.noreply.github.com>
2022-10-20 19:34:34 +0300

    Update Cube.h

--- semester_3/Rubik-s-Cube/Cube.h
+++ semester_3/Rubik-s-Cube/Cube.h
@@ -1,24 +1,27 @@
 #pragma once
 
 #include "Face.h"
+#include <vector>
 
+
+/// This class represents the cube entity
 class Cube
 {
 private:
 
-	int cubeSize;
+	int	cubeSize; 
 	Face faces[6];
 
-	void changeOrientToLeft(int neighbor);
+	void changeOrientToLeft(int neighbor); 
 	void changeOrientToRight(int neighbor);
 	void rotateY(int face, int line, string currentNaighbor);
 	void rotateX(int face, int line, string currentNaighbor);
 
 public:
 
-	Cube(int bars[6][2][3][3]);
-	void print();
-	void rotate(int face, int direction, int line);
-	bool check();
+	Cube(vector<vector<vector<vector<int>>>>&);		///<\brief Constructor takes as input a multidimensional vector that stores all bars
+	void print();	///<\brief Print the scan of the cube in the console
+	void rotate(int face, int direction, int line);		///<\brief Rotate the face of the cube
+	bool check();	///<\brief Check if cube is built
 };
 
